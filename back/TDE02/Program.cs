0a20e1268958c0046e24069499f8373932e4d4c9
carlos esfolha <duduesfolha@rede.ulbra.br>
2025-08-05 21:37:01 -0300

    adicionando back

---
+++ back/TDE02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+Console.WriteLine("digite um numero:");
+Double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("digite o segundo numero:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double soma = num1 + num2;
+double sub = num1 - num2;
+double mul = num1 * num2;
+double div = num1 / num2;
+
+
+Console.WriteLine("a soma de " + num1 + "E" + num2 + "é igual a " + soma);
+Console.WriteLine($"a subtração de{num1} e {num2} é igual a {sub}");
+Console.WriteLine("a multiplicação de {0} com {1} é igual a {2} ", num1, num2, mul);
---
+++ back/tipos/Program.cs
@@ -0,0 +1,34 @@
+﻿/*int x = 10;
+
+double y = x;
+double pi = 3.14;
+
+int num = (int)pi;
+
+string numerotexto = "123";
+int numero = Convert.ToInt32(numerotexto);
+
+string nome = "lucas";
+int idade = 33;
+
+
+//concatena
+string mensagem = "ola meu nome e " + nome + " e eu tenho " + idade + " anos.";
+
+//interpolaçao
+string mensagemInterpolada = $"ola meu nome e {nome} e eu tenho {idade} anos.";
+
+// spaceholder
+string mensagemplaceholder = string.Format("ola meu nome e {0} e eu tenho {1} anos.", nome, idade);
+
+Console.WriteLine(mensagem);
+Console.WriteLine(mensagemInterpolada);
+Console.WriteLine(mensagemplaceholder);
+*/
+Console.WriteLine("digite seu nome:");
+string nome = Console.ReadLine();
+
+Console.WriteLine("digite sua idade:");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"meu nome é {nome} e minha idade é {idade}");
